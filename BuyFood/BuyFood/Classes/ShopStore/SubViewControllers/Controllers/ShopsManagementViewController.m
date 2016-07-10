2e3c09df210b9275ee5beccb9873040aed8dddb9
Tracy-deng <dave.deng@cardinfolink.com>
2016-07-10 15:27:20 +0800

    修改下 ， 商户管理

--- BuyFood/BuyFood/Classes/ShopStore/SubViewControllers/Controllers/ShopsManagementViewController.m
+++ BuyFood/BuyFood/Classes/ShopStore/SubViewControllers/Controllers/ShopsManagementViewController.m
@@ -28,7 +28,7 @@
 {
     [super viewDidLoad];
     self.title = @"商品管理";
-    [self.view setBackgroundColor:[UIColor colorWithWhite:0.795 alpha:1.000]];
+    [self.view setBackgroundColor:[UIColor colorWithWhite:0.915 alpha:1.000]];
     [self creatTableView];
     
     /**
@@ -61,7 +61,7 @@
         make.top.equalTo(self.view).offset(64);
         make.left.equalTo(_selectedTableView.mas_right);
         make.right.equalTo(self.view);
-        make.height.equalTo(@(SCREEN_HEIGHT));
+        make.height.equalTo(@(SCREEN_HEIGHT - 200));
     }];
     
     
@@ -75,6 +75,39 @@
     self.mainTableView.tableFooterView = view;
     
     [self.mainTableView registerClass:[ShopTableViewCell class] forCellReuseIdentifier:@"reuse"];
+    
+    
+   
+    
+    UIButton * addBtn = [UIButton buttonWithType:(UIButtonTypeSystem)];
+    addBtn.backgroundColor = greenColor;
+    [addBtn setTitleColor:[UIColor whiteColor] forState:(UIControlStateNormal)];
+    [self.view addSubview:addBtn];
+    [addBtn setTitle:@"添加" forState:(UIControlStateNormal)];
+    [addBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(self.view.mas_bottom).offset(-30);
+        make.left.equalTo(self.selectedTableView.mas_right).offset(10);
+        make.width.equalTo(@100);
+        make.height.equalTo(@40);
+    }];
+    addBtn.layer.masksToBounds = YES;
+    addBtn.layer.cornerRadius = 5;
+    [addBtn addTarget:self action:@selector(didAddBtn:) forControlEvents:(UIControlEventTouchUpInside)];
+    
+    UIButton * reorderBtn = [UIButton buttonWithType:(UIButtonTypeSystem)];
+    reorderBtn.backgroundColor = greenColor;
+    [reorderBtn setTitleColor:[UIColor whiteColor] forState:(UIControlStateNormal)];
+    [self.view addSubview:reorderBtn];
+    [reorderBtn setTitle:@"排序" forState:(UIControlStateNormal)];
+    [reorderBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(addBtn);
+        make.right.equalTo(self.view).offset(-10);
+        make.width.equalTo(@100);
+        make.height.equalTo(@40);
+    }];
+    [reorderBtn addTarget:self action:@selector(reordeBtn:) forControlEvents:(UIControlEventTouchUpInside)];
+    reorderBtn.layer.masksToBounds = YES;
+    reorderBtn.layer.cornerRadius = 5;
     
     
 }
@@ -97,7 +130,7 @@
         
         UIView *headView = [[UIView alloc]init];
 
-        headView.backgroundColor = [UIColor colorWithWhite:0.795 alpha:1.000];
+        headView.backgroundColor = [UIColor colorWithWhite:0.915 alpha:1.000];
         
         headView.frame = CGRectMake(0, 0, SCREEN_WIDTH - 100, 40);
         UIButton * hotBtn = [UIButton buttonWithType:(UIButtonTypeSystem)];
@@ -106,16 +139,6 @@
         hotBtn.frame = CGRectMake(10, 10, 30, 30);
         [hotBtn setTitleColor:[UIColor blackColor] forState:(UIControlStateNormal)];
         
-        UIButton *  changeBtn = [UIButton buttonWithType:(UIButtonTypeSystem)];
-        [changeBtn setTitle:@"修改" forState:(UIControlStateNormal)];
-        [changeBtn setTitleColor:greenColor forState:(UIControlStateNormal)];
-        [headView addSubview:changeBtn];
-        [changeBtn mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.top.equalTo(hotBtn);
-            make.right.equalTo(headView).offset(-5);
-            make.width.equalTo(@30);
-            make.height.equalTo(@30);
-        }];
         
         return headView;
     }
@@ -124,60 +147,6 @@
     
 }
 
-// 添加区尾
-- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section{
-    
-    if ([tableView isEqual:_mainTableView]) {
-        
-        UIView * footView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH - 100, 40)];
-        
-        UIView *lineView = [[UIView alloc]init];
-        lineView.backgroundColor = [UIColor colorWithWhite:0.528 alpha:1.000];
-        [footView addSubview:lineView];
-        lineView.alpha = 0.3;
-        [lineView mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.top.equalTo(footView);
-            make.left.right.equalTo(footView);
-            make.height.equalTo(@0.5);
-        }];
-        
-        UIButton * addBtn = [UIButton buttonWithType:(UIButtonTypeSystem)];
-        addBtn.backgroundColor = greenColor;
-        [addBtn setTitleColor:[UIColor whiteColor] forState:(UIControlStateNormal)];
-        [footView addSubview:addBtn];
-        [addBtn setTitle:@"添加" forState:(UIControlStateNormal)];
-        [addBtn mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.top.equalTo(footView).offset(10);
-            make.left.equalTo(footView).offset(10);
-            make.width.equalTo(@80);
-            make.height.equalTo(@30);
-        }];
-        addBtn.layer.masksToBounds = YES;
-        addBtn.layer.cornerRadius = 4;
-        footView.backgroundColor = [UIColor colorWithWhite:0.915 alpha:1.000];
-        [addBtn addTarget:self action:@selector(didAddBtn:) forControlEvents:(UIControlEventTouchUpInside)];
-        
-        UIButton * reorderBtn = [UIButton buttonWithType:(UIButtonTypeSystem)];
-        reorderBtn.backgroundColor = greenColor;
-        [reorderBtn setTitleColor:[UIColor whiteColor] forState:(UIControlStateNormal)];
-        [footView addSubview:reorderBtn];
-        [reorderBtn setTitle:@"排序" forState:(UIControlStateNormal)];
-        [reorderBtn mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.top.equalTo(footView).offset(10);
-            make.right.equalTo(footView).offset(-10);
-            make.width.equalTo(@80);
-            make.height.equalTo(@30);
-        }];
-        [reorderBtn addTarget:self action:@selector(reordeBtn:) forControlEvents:(UIControlEventTouchUpInside)];
-        reorderBtn.layer.masksToBounds = YES;
-        reorderBtn.layer.cornerRadius = 4;
-        
-        return footView;
-    }
-    
-    return 0;
-    
-}
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
@@ -256,8 +225,12 @@
 // 点击 mainTableView 进入商品详情
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    ShopDetailViewController *detailVC =  [[ShopDetailViewController alloc]init];
-    [self.navigationController pushViewController:detailVC animated:YES];
+    if([tableView isEqual:_mainTableView])
+    {
+        ShopDetailViewController *detailVC =  [[ShopDetailViewController alloc]init];
+        [self.navigationController pushViewController:detailVC animated:YES];
+    }
+   
     
 }
 // 每个tableView 的高度
@@ -282,16 +255,7 @@
         return 0;
 }
 
-- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
-{
-    if ([tableView isEqual:_mainTableView]) {
-        
-        return 60;
-        
-    }else
-        
-        return 0;
-}
+
 
 #pragma mark -- 让_mainTableView 进入编辑 并且排序
 - (void)reordeBtn:(UIButton *)sender
