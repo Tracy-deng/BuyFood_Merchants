c09f428d3f3d128c52948af7a6541729c6dbacf6
Location_Heart <Location_Heart@163.com>
2016-07-26 20:44:47 +0800

    修改添加拼团和户外活动界面。

--- BuyFood/BuyFood/Classes/ShopStore/SubViewControllers/Controllers/Controller/AddControllers/FightGroupsViewController.m
+++ BuyFood/BuyFood/Classes/ShopStore/SubViewControllers/Controllers/Controller/AddControllers/FightGroupsViewController.m
@@ -8,6 +8,8 @@
 
 #import "FightGroupsViewController.h"
 #import "AddShopsCell.h"
+#import "MHActionSheet.h"
+#import "MHDatePicker.h"
 
 #define Start_X self.view.frame.size.width * 0.05           // 第一个按钮的X坐标
 #define Start_Y self.view.frame.size.height - (self.view.frame.size.height * 0.19) - (self.view.frame.size.width * 0.27)          // 第一个按钮的Y坐标
@@ -16,8 +18,24 @@
 
 @interface FightGroupsViewController ()<UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate>
 
-@property (nonatomic, strong) UITableView* tableView;
-@property (nonatomic, strong) UIButton* imageViewBtn;
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) UIButton *imageViewBtn;
+/** 单位 */
+@property (nonatomic, strong) NSString *unitStr;
+/** 商品名称 */
+@property (nonatomic, strong) NSString *productname;
+/** 商品原价 */
+@property (nonatomic, strong) NSString *productprice;
+/** 拼购价 */
+@property (nonatomic, strong) NSString *productprice2;
+/** 人数限制 */
+@property (nonatomic, strong) NSString *peopleNum;
+/** 商品重量 */
+@property (nonatomic, strong) NSString *shopsWeight;
+/** 时间选择器 */
+@property (strong, nonatomic) MHDatePicker *selectTimePicker;
+/** 活动时间 */
+@property (nonatomic, strong) NSString *fightGroupsTime;
 
 @end
 
@@ -71,12 +89,12 @@
     AddShopsCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
     if (!cell)
     {
-        if (indexPath.row == 0 || indexPath.row == 1 || indexPath.row == 2 || indexPath.row == 3)
+        if (indexPath.row == 0 || indexPath.row == 1 || indexPath.row == 2 || indexPath.row == 3 || indexPath.row == 5)
         {
             cell = [[AddShopsCell alloc] initWithInputCellStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
             cell.contentTextField.tag = indexPath.row;
             cell.contentTextField.delegate = self;
-            //            [cell.contentTextField addTarget:self action:@selector(changeValue:) forControlEvents:UIControlEventEditingChanged];
+            [cell.contentTextField addTarget:self action:@selector(changeValue:) forControlEvents:UIControlEventEditingChanged];
             switch (cell.contentTextField.tag)
             {
                 case 0:
@@ -91,6 +109,9 @@
                 case 3:
                     [cell setTitleLabel:@"人数限制:" andContentTextFieldPlaceholder:@"请输入限制人数"];
                     break;
+                case 5:
+                    [cell setTitleLabel:@"重量:" andContentTextFieldPlaceholder:@"请输入商品重量"];
+                    break;
                 default:
                     break;
             }
@@ -98,17 +119,14 @@
         else
         {
             cell = [[AddShopsCell alloc] initWithChooseCellStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
+            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
             if (indexPath.row == 4)
             {
-                [cell setChooseTitleLabel:@"单位:" andContentLabel:@"单位"];
-            }
-            else if (indexPath.row == 5)
-            {
-                [cell setChooseTitleLabel:@"重量:" andContentLabel:@"重量"];
+                [cell setChooseTitleLabel:@"单位:" andContentLabel:self.unitStr];
             }
             else
             {
-                [cell setChooseTitleLabel:@"活动时间:" andContentLabel:@"活动时间"];
+                [cell setChooseTitleLabel:@"活动时间:" andContentLabel:self.fightGroupsTime];
             }
         }
     }
@@ -120,6 +138,70 @@
     return 60;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    AddShopsCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    if (indexPath.row == 4)
+    {
+        NSArray *array = @[@"克",
+                           @"份"];
+        MHActionSheet *actionSheet = [[MHActionSheet alloc] initSheetWithTitle:@"选择商品单位" style:MHSheetStyleWeiChat itemTitles:array];
+        actionSheet.cancleTitle = @"取消选择";
+        [actionSheet didFinishSelectIndex:^(NSInteger index, NSString *title)
+         {
+             self.unitStr = title;
+             [cell setChooseTitleLabel:@"单位:" andContentLabel:self.unitStr];
+             [self.tableView reloadData];
+         }];
+    }
+    if (indexPath.row == 6)
+    {
+        _selectTimePicker = [[MHDatePicker alloc] init];
+        __weak typeof(self) weakSelf = self;
+        [_selectTimePicker didFinishSelectedDate:^(NSDate *selectedDate)
+         {
+             //             self.fightGroupsTime = [weakSelf dateStringWithDate:selectedDate DateFormat:@"YYYY/MM/dd hh:mm"];
+             self.fightGroupsTime = [weakSelf dateStringWithDate:selectedDate DateFormat:@"YYYY/MM/dd"];
+             [cell setChooseTitleLabel:@"活动时间:" andContentLabel:self.fightGroupsTime];
+             [self.tableView reloadData];
+         }];
+    }
+}
+- (NSString *)dateStringWithDate:(NSDate *)date DateFormat:(NSString *)dateFormat
+{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:dateFormat];
+    [dateFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"zh_CN"]];
+    NSString *str = [dateFormatter stringFromDate:date];
+    return str ? str : @"";
+}
+#pragma textField输入
+- (void)changeValue:(UITextField *)textField
+{
+    switch (textField.tag)
+    {
+        case 0:
+            self.productname = textField.text;
+            break;
+        case 1:
+            self.productprice = textField.text;
+            break;
+        case 2:
+            self.productprice2 = textField.text;
+            break;
+        case 3:
+            self.peopleNum = textField.text;
+            break;
+        case 5:
+            self.shopsWeight = textField.text;
+            break;
+        default:
+            break;
+    }
+}
+
+/** 添加图片 */
 - (void)setAddImageView
 {
     for (int i = 0; i < 3; i ++ )
@@ -140,61 +222,57 @@
     NSLog(@"%ld", sender.tag);
 }
 
-/**
- //    if (indexPath.row == 0)
- //    {
- //        static NSString* ID = @"Cell";
- //        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
- //
- //        if (!cell)
- //        {
- //            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:ID];
- //        }
- //        cell.textLabel.text = @"商品名称:";
- //        cell.textLabel.textColor = [UIColor colorWithRed:102 / 255.0 green:102 / 255.0 blue:102 / 255.0 alpha:1];
- //        cell.textLabel.font = [UIFont fontWithName:@"PingFangSC-Regular" size:20];
- //        UITextField* textField = [[UITextField alloc] init];
- //        textField.placeholder = @"输入商品名称";
- //        textField.textColor = [UIColor colorWithRed:102 / 255.0 green:102 / 255.0 blue:102 / 255.0 alpha:1];
- //        textField.font = [UIFont fontWithName:@"PingFangSC-Regular" size:20];
- //        [cell.contentView addSubview:textField];
- //        [textField mas_makeConstraints:^(MASConstraintMaker *make) {
- //            make.left.mas_equalTo(cell.contentView.mas_left).offset(120);
- //            make.top.mas_equalTo(cell.contentView.mas_top).offset(15);
- //            make.width.equalTo(@150);
- //            make.height.equalTo(@30);
- //        }];
- //        return cell;
- //    }
- //    else
- //    {
- //        SecondsKillCell* cell = [SecondsKillCell cellWithTableView:tableView];
- //        if (indexPath.row == 1)
- //        {
- //            [cell setTitleLabel:@"拼购价：" andContentLabel:@"0.00"];
- //        }
- //        else if (indexPath.row == 2)
- //        {
- //            [cell setTitleLabel:@"原价：" andContentLabel:@"0.00"];
- //        }
- //        else if (indexPath.row == 3)
- //        {
- //            [cell setTitleLabel:@"人数限制" andContentLabel:@""];
- //        }
- //        else if (indexPath.row == 4)
- //        {
- //            [cell setTitleLabel:@"单位" andContentLabel:@"份"];
- //        }
- //        else if (indexPath.row == 5)
- //        {
- //            [cell setTitleLabel:@"重量：" andContentLabel:@""];
- //        }
- //        else
- //        {
- //            [cell setTitleLabel:@"活动时间:" andContentLabel:@""];
- //        }
- //        return cell;
- //    }
- */
+#pragma mark -- 点击键盘上的return按钮  收起键盘
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    
+    [textField resignFirstResponder];
+    
+    return YES;
+    
+}
+
+// 点击屏幕空白  收起键盘
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];
+}
+
+// 在UITextField 编辑之前调用方法  视图上移
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    [self packUpDownTextField:textField isShow:YES];
+    
+}
+// 编辑结束 视图返回
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    
+    [self packUpDownTextField:textField isShow:NO];
+}
+- (void)packUpDownTextField:(UITextField *)textField isShow:(BOOL)isShow
+{
+    if (textField.tag == 0 || textField.tag == 1 || textField.tag == 2 || textField.tag == 3 || textField.tag == 5) {
+        
+        //设置动画的名字
+        [UIView beginAnimations:@"Animation" context:nil];
+        //设置动画的间隔时间
+        [UIView setAnimationDuration:0.20];
+        //??使用当前正在运行的状态开始下一段动画
+        [UIView setAnimationBeginsFromCurrentState: YES];
+        if (isShow == YES) {
+            //设置视图下移动的位移
+            self.view.frame = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y - 0.3*SCREEN_HEIGHT, self.view.frame.size.width, self.view.frame.size.height);
+        }else{
+            //设置视图上移动的位移
+            self.view.frame = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y + 0.3*SCREEN_HEIGHT, self.view.frame.size.width, self.view.frame.size.height);
+            
+        }
+        
+        //设置动画结束
+        [UIView commitAnimations];
+    }else{
+        return ;
+    }
+}
+
 
 @end
--- BuyFood/BuyFood/Classes/ShopStore/SubViewControllers/Controllers/Controller/AddControllers/OutdoorActivitiesViewController.m
+++ BuyFood/BuyFood/Classes/ShopStore/SubViewControllers/Controllers/Controller/AddControllers/OutdoorActivitiesViewController.m
@@ -6,19 +6,31 @@
 //  Copyright © 2016年 huangdongchun. All rights reserved.
 //
 
-#import "OutdoorActivitiesViewController.h" 
-#import "SecondsKillCell.h"
-
+#import "OutdoorActivitiesViewController.h"
+#import "AddShopsCell.h"
+#import "MHDatePicker.h"
 
 #define Start_X self.view.frame.size.width * 0.05           // 第一个按钮的X坐标
 #define Start_Y self.view.frame.size.height - (self.view.frame.size.height * 0.19) - (self.view.frame.size.width * 0.27)          // 第一个按钮的Y坐标
 #define Width_Space self.view.frame.size.width * 0.05        // 2个按钮之间的横间距
 #define Button_Width self.view.frame.size.width * 0.27 //宽
 
-@interface OutdoorActivitiesViewController ()<UITableViewDelegate, UITableViewDataSource>
+@interface OutdoorActivitiesViewController ()<UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate>
 
 @property (nonatomic, strong) UITableView* tableView;
 @property (nonatomic, strong) UIButton* imageViewBtn;
+/** 活动名称 */
+@property (nonatomic, strong) NSString *fightGroupsName;
+/** 报名费 */
+@property (nonatomic, strong) NSString *fightGroupsPrice;
+/** 人数限制 */
+@property (nonatomic, strong) NSString *peopleNum;
+/** 活动描述 */
+@property (nonatomic, strong) NSString *fightGroupsDescribe;
+/** 活动时间 */
+@property (nonatomic, strong) NSString *fightGroupsTime;
+/** 时间选择器 */
+@property (strong, nonatomic) MHDatePicker *selectTimePicker;
 
 @end
 
@@ -68,53 +80,42 @@
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    if (indexPath.row == 0)
+    NSString *ID = [NSString stringWithFormat:@"ID %ld", indexPath.row];
+    AddShopsCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    if (!cell)
     {
-        static NSString* ID = @"Cell";
-        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
-        
-        if (!cell)
+        if (indexPath.row == 0 || indexPath.row == 1 || indexPath.row == 2 || indexPath.row == 4)
         {
-            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:ID];
-        }
-        cell.textLabel.text = @"活动名称:";
-        cell.textLabel.textColor = [UIColor colorWithRed:102 / 255.0 green:102 / 255.0 blue:102 / 255.0 alpha:1];
-        cell.textLabel.font = [UIFont fontWithName:@"PingFangSC-Regular" size:20];
-        UITextField* textField = [[UITextField alloc] init];
-        textField.placeholder = @"输入活动名称";
-        textField.textColor = [UIColor colorWithRed:102 / 255.0 green:102 / 255.0 blue:102 / 255.0 alpha:1];
-        textField.font = [UIFont fontWithName:@"PingFangSC-Regular" size:20];
-        [cell.contentView addSubview:textField];
-        [textField mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.left.mas_equalTo(cell.contentView.mas_left).offset(120);
-            make.top.mas_equalTo(cell.contentView.mas_top).offset(15);
-            make.width.equalTo(@150);
-            make.height.equalTo(@30);
-        }];
-        return cell;
-    }
-    else
-    {
-        
-        SecondsKillCell* cell = [SecondsKillCell cellWithTableView:tableView];
-        if (indexPath.row == 1)
-        {
-            [cell setTitleLabel:@"报名费：" andContentLabel:@"0.00"];
-        }
-        else if (indexPath.row == 2)
-        {
-            [cell setTitleLabel:@"人数限制：" andContentLabel:@"0.00"];
-        }
-        else if (indexPath.row == 3)
-        {
-            [cell setTitleLabel:@"活动时间" andContentLabel:@""];
+            cell = [[AddShopsCell alloc] initWithInputCellStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
+            cell.contentTextField.tag = indexPath.row;
+            cell.contentTextField.delegate = self;
+            [cell.contentTextField addTarget:self action:@selector(changeValue:) forControlEvents:UIControlEventEditingChanged];
+            switch (cell.contentTextField.tag)
+            {
+                case 0:
+                    [cell setTitleLabel:@"活动名称:" andContentTextFieldPlaceholder:@"请输入活动名称"];
+                    break;
+                case 1:
+                    [cell setTitleLabel:@"报名费:" andContentTextFieldPlaceholder:@"请输入活动报名费"];
+                    break;
+                case 2:
+                    [cell setTitleLabel:@"人数限制:" andContentTextFieldPlaceholder:@"请输入限制人数"];
+                    break;
+                case 4:
+                    [cell setTitleLabel:@"活动描述:" andContentTextFieldPlaceholder:@"二十字以内"];
+                    break;
+                default:
+                    break;
+            }
         }
         else
         {
-            [cell setTitleLabel:@"活动描述" andContentLabel:@""];
+            cell = [[AddShopsCell alloc] initWithChooseCellStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
+            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            [cell setChooseTitleLabel:@"活动时间:" andContentLabel:self.fightGroupsTime];
         }
-        return cell;
     }
+    return cell;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -122,6 +123,55 @@
     return 60;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    AddShopsCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    if (indexPath.row == 3)
+    {
+        _selectTimePicker = [[MHDatePicker alloc] init];
+        __weak typeof(self) weakSelf = self;
+        [_selectTimePicker didFinishSelectedDate:^(NSDate *selectedDate)
+         {
+             //self.fightGroupsTime = [weakSelf dateStringWithDate:selectedDate DateFormat:@"YYYY/MM/dd hh:mm"];
+             self.fightGroupsTime = [weakSelf dateStringWithDate:selectedDate DateFormat:@"YYYY/MM/dd"];
+             [cell setChooseTitleLabel:@"活动时间:" andContentLabel:self.fightGroupsTime];
+             [self.tableView reloadData];
+         }];
+    }
+}
+- (NSString *)dateStringWithDate:(NSDate *)date DateFormat:(NSString *)dateFormat
+{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:dateFormat];
+    [dateFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"zh_CN"]];
+    NSString *str = [dateFormatter stringFromDate:date];
+    return str ? str : @"";
+}
+#pragma textField输入
+- (void)changeValue:(UITextField *)textField
+{
+    HDCLog(@"textField输入%ld,%@",textField.tag,textField.text);
+    switch (textField.tag)
+    {
+        case 0:
+            self.fightGroupsName = textField.text;
+            break;
+        case 1:
+            self.fightGroupsPrice = textField.text;
+            break;
+        case 2:
+            self.peopleNum = textField.text;
+            break;
+        case 4:
+            self.fightGroupsDescribe = textField.text;
+            break;
+        default:
+            break;
+    }
+}
+
+#pragma 添加图片
 - (void)setAddImageView
 {
     for (int i = 0; i < 3; i ++ )
@@ -140,6 +190,57 @@
 - (void)imageViewBtnClick:(UIButton* )sender
 {
     NSLog(@"%ld", sender.tag);
+}
+#pragma mark -- 点击键盘上的return按钮  收起键盘
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    
+    [textField resignFirstResponder];
+    
+    return YES;
+    
+}
+
+// 点击屏幕空白  收起键盘
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];
+}
+
+// 在UITextField 编辑之前调用方法  视图上移
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    [self packUpDownTextField:textField isShow:YES];
+    
+}
+// 编辑结束 视图返回
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    
+    [self packUpDownTextField:textField isShow:NO];
+}
+- (void)packUpDownTextField:(UITextField *)textField isShow:(BOOL)isShow
+{
+    if (textField.tag == 0 || textField.tag == 1 || textField.tag == 2  || textField.tag == 4) {
+        
+        //设置动画的名字
+        [UIView beginAnimations:@"Animation" context:nil];
+        //设置动画的间隔时间
+        [UIView setAnimationDuration:0.20];
+        //??使用当前正在运行的状态开始下一段动画
+        [UIView setAnimationBeginsFromCurrentState: YES];
+        if (isShow == YES) {
+            //设置视图下移动的位移
+            self.view.frame = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y - 0.3*SCREEN_HEIGHT, self.view.frame.size.width, self.view.frame.size.height);
+        }else{
+            //设置视图上移动的位移
+            self.view.frame = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y + 0.3*SCREEN_HEIGHT, self.view.frame.size.width, self.view.frame.size.height);
+            
+        }
+        
+        //设置动画结束
+        [UIView commitAnimations];
+    }else{
+        return ;
+    }
 }
 
 
